3da96262ed5fa1e124741166f60367775780fa17
SatyaSampreeth <59055777+SatyaSampreeth@users.noreply.github.com>
2023-10-06 22:42:56 -0500

    Time: 63 ms (16.67%), Space: 17.4 MB (20.78%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,10 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        if sorted(s)== sorted(t):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s)!=len(t):
+            return False
+        elif sorted(s)==sorted(t):
             return True
         return False
         
+            
